b138b1898647d4c5dc154254d3e129af3e19d067
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 04:28:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   FROM users
   INNER JOIN pledges
   ON user.id = pledges.user_id
-  GROUP BY user.name
+  GROUP BY users.name
   "
 end
 
