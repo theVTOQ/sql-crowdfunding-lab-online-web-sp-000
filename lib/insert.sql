884327a3da8f25924b1bdbdc534c1ea7d21687d1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 03:59:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,7 @@
-INSERT INTO projects (title, funding_goal) VALUES ("Eden", 100000);
-INSERT INTO projects (title, funding_goal) VALUES ("Arcadia", 50000);
+INSERT INTO projects (title, funding_goal) VALUES ("Eden
+  INSERT INTO projects (title, funding_goal) VALUES ("Eden", 100000);
+INSERT INTO projects (title, funding_goal) VALUES ("Arcadia
+  INSERT INTO projects (title, funding_goal) VALUES ("Arcadia", 50000);
 INSERT INTO projects (title, funding_goal) VALUES ("Optimus Prime", 300002200);
 INSERT INTO projects (title, funding_goal) VALUES ("Archeology", 513248);
 INSERT INTO projects (title, funding_goal) VALUES ("Starkiller", 69000);
@@ -8,3 +10,30 @@ INSERT INTO projects (title, funding_goal) VALUES ("GOT S8 Rewrite", 8904561);
 INSERT INTO projects (title, funding_goal) VALUES ("SpaceX", 89513215);
 INSERT INTO projects (title, funding_goal) VALUES ("Moon Base", 59412389);
 INSERT INTO projects (title, funding_goal) VALUES ("Space Force", 1000000000);
+
+INSERT INTO users (name) VALUES ("Marcus");
+INSERT INTO users (name) VALUES ("Sven");
+INSERT INTO users (name) VALUES ("Peter");
+INSERT INTO users (name) VALUES ("Piotr");
+INSERT INTO users (name) VALUES ("Ishmael");
+INSERT INTO users (name) VALUES ("Raul");
+INSERT INTO users (name) VALUES ("Ignacius");
+INSERT INTO users (name) VALUES ("Christopher");
+INSERT INTO users (name) VALUES ("Rodrigo");
+INSERT INTO users (name) VALUES ("Colby");
+INSERT INTO users (name) VALUES ("Fabio");
+INSERT INTO users (name) VALUES ("Arcturus");
+INSERT INTO users (name) VALUES ("Hercules");
+INSERT INTO users (name) VALUES ("Electra");
+INSERT INTO users (name) VALUES ("Orpheus");
+INSERT INTO users (name) VALUES ("Morpheus");
+INSERT INTO users (name) VALUES ("Ricardo");
+INSERT INTO users (name) VALUES ("Steven");
+INSERT INTO users (name) VALUES ("Plato");
+INSERT INTO users (name) VALUES ("Agamemnon");
+INSERT INTO users (name) VALUES ("Rey");
+
+
+
+
+
