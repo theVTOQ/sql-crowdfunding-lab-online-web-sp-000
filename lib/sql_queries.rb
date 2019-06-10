5ce3857f0d4e48018c811b7feabc5c65f402ebe1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 04:34:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,11 +35,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT user.name, pledges.amount
+  "SELECT users.name, pledges.amount
   FROM users
   INNER JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY user.name
+  GROUP BY users.name
   ORDER BY pledges.amount"
 end
 
