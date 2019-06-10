e7859a39049bec24ea00015547d2190850604553
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 03:52:41 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO projects (title, funding_goal) VALUES ("Eden", 100000);
+INSERT INTO projects (title, funding_goal) VALUES ("Arcadia", 50000);
+INSERT INTO projects (title, funding_goal) VALUES ("Optimus Prime", 300002200);
+INSERT INTO projects (title, funding_goal) VALUES ("Archeology", 513248);
