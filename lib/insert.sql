f831e3b49544e0971a36567a64468a6dce86848e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 04:24:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,5 @@
-INSERT INTO projects (title, funding_goal) VALUES ("Eden
-  INSERT INTO projects (title, funding_goal) VALUES ("Eden", 100000);
-INSERT INTO projects (title, funding_goal) VALUES ("Arcadia
-  INSERT INTO projects (title, funding_goal) VALUES ("Arcadia", 50000);
+INSERT INTO projects (title, funding_goal) VALUES ("Eden", 100000);
+INSERT INTO projects (title, funding_goal) VALUES ("Arcadia", 50000);
 INSERT INTO projects (title, funding_goal) VALUES ("Optimus Prime", 300002200);
 INSERT INTO projects (title, funding_goal) VALUES ("Archeology", 513248);
 INSERT INTO projects (title, funding_goal) VALUES ("Starkiller", 69000);
